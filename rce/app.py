50e20ac0577ebe53468426f1b6ab8bd9d05f6487
Héctor Romero López <i72roloh@uco.es>
2021-10-15 12:33:28 +0200

    simple app for rce done

---
+++ rce/app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+import os
+app = Flask(__name__)
+
+
+@app.route("/read/<file>/", methods=["GET"])
+def read_file(file):
+    command = f"cat {file}"
+    return os.popen(command).read()
+
+
+@app.route("/")
+def index():
+    return ""
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
