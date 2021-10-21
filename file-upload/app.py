dc378f23ae9e0eef5327f5516bde9818ff17194a
Héctor Romero López <i72roloh@uco.es>
2021-10-21 11:45:02 +0200

    Added app

--- file-upload/app.py
+++ file-upload/app.py
@@ -1,14 +1,33 @@
 from flask import Flask
 from flask import render_template
+from flask import request
+from flask import redirect
+import os
 app = Flask(__name__, template_folder="templates")
+UPLOAD_FOLDER = "media"
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
-@app.route("", methods=["GET","POST"])
+@app.route("/", methods=["GET","POST"])
 def index():
+    if request.method == "POST":
+        file = request.files['file']
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+        return redirect(f"media/{file.filename}")
 
     return render_template(
        "index.html",
     )
 
 
-app.run(addr="0.0.0.0", port=8000)
+@app.route("/image/<image>", methods=["GET"])
+def images(image):
+    if image in os.listdir("media"):
+        return image
+    else:
+        return "None"
+
+
+
+app.run(host="0.0.0.0", port=8000)
