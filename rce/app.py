3166b08a306e7e1ffd07c31a1d27c6016e1f249a
Héctor Romero López <i72roloh@uco.es>
2021-10-21 11:45:12 +0200

    Added index

--- rce/app.py
+++ rce/app.py
@@ -11,7 +11,8 @@ def read_file(file):
 
 @app.route("/")
 def index():
-    return ""
+    return "[!] Welcome to system file reader app"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
